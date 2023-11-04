d9150e9620de770f518d1298ed12446761e911c0
erfan <1400erfan1400@gmail.com>
2023-11-04 17:49:23 +0330

    Rename folders

---
+++ Project 1/Project1.py
@@ -0,0 +1,18 @@
+kms = int(input("How many kms did you drive : ")) #Ask from user
+
+print("You drived " + str(kms)  + "/" + "kms or " + str(kms * 1000) + " metrs" ) #Khoroji
+
+#...........................................
+
+num1 = int(input("Enter your first number : "))
+
+num2 = int(input("Enter your secend number : "))
+
+print(num1 , ' + ' , num2 , " = " , num1 + num2)#Jam
+print(num1 , ' - ' , num2 , " = " , num1 - num2)#Menha
+print(num1 , ' * ' , num2 , " = " , num1 * num2)#Zarb
+print(num1 , ' / ' , num2 , " = " , num1 / num2)#Taghsim
+
+print(num1 , ' be tavane ' , num2 , " = " , num1 ** num2)#Tavan
+
+print(num1 * num2 - ((num2 + num2 / num2) / num1) + num2)#Control kardan ba ()
