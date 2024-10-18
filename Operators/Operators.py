973284009e31cb72660384cff17394f27e4cd20c
erfan <128138389+erfan-omran@users.noreply.github.com>
2024-10-19 01:36:12 +0330

    Add files via upload

---
+++ Operators/Operators.py
@@ -0,0 +1,13 @@
+
+num1 = int(input("Enter num1 : "))
+num2 = int(input("\nEnter num2 : "))
+
+#------------------------------
+print("\n")
+print(num1 + num2)
+print(num1 - num2)
+#--------
+print(num1 * num2)
+print(num1 / num2)
+#--------
+print(num1 ** num2)
